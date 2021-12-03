a693946b30609b0c1e356f0d7456528171fc2a1a
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-12-03 17:39:27 -0600

    first commit

---
+++ resources/views/empleado/form.blade.php
@@ -0,0 +1,14 @@
+{{-- formulario que tendra los datos --}}
+<label for="">Nombre:</label>
+<input type="text" name="nombre"  value="{{isset($empleado->nombre)?$empleado->nombre:''}}" placeholder="Nombre">
+<br>
+<label for="">Apellido:</label>
+<input type="text" name="apellido" value="{{isset($empleado->apellido)?$empleado->apellido:''}}" placeholder="apellido">
+<br>
+<label for="">Puesto:</label>
+<input type="text" name="puesto" value="{{isset($empleado->puesto)?$empleado->puesto:''}}" placeholder="puesto">
+<br>
+<label for="">Email:</label>
+<input type="email" name="email"  value="{{isset($empleado->email)?$empleado->email:''}}" placeholder="email">
+<br>
+<input type="submit" value="Guardar Datos">
---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,35 @@
+{{-- Mostrar lista de empleados --}}
+<table class="table table-light">
+    {{-- cabezera --}}
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Puesto</th>
+            <th>Email</th>
+            <th>Acciones</th>
+            
+        </tr>
+    </thead>
+    {{-- Cuerpo --}}
+    <tbody>
+        @foreach ($empleados as $empleado)
+        <tr>
+            <td>{{$empleado->id}}</td>
+            <td>{{$empleado->nombre}}</td>
+            <td>{{$empleado->apellido}}</td>
+            <td>{{$empleado->puesto}}</td>
+            <td>{{$empleado->email}}</td>
+            <td><a href="{{url('/empleado/'.$empleado->id.'/edit')}}">Editar</a> 
+                | 
+                <form action="{{ url('/empleado/'.$empleado->id) }}" method="post">
+                    @csrf
+                    {{method_field('DELETE')}}
+                    <input type="submit" onclick="return confirm('¿Quieres Borrar?')" value="Borrar">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
